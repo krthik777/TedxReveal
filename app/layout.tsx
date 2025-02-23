f7451f3c6afa44ec7ad5f559c7ff9eed424f01ff
krthik777 <karthikv9778@gmail.com>
2025-02-23 16:56:31 +0530

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#ffffff"></meta>
+      </head>
       <body className={spaceGrotesk.className}>{children}</body>
     </html>
   );
